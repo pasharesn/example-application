6b424216d624ff03fa85e735bd86979e9f85349a
pasharesn <pasha@areionsec.com>
2020-10-20 14:13:29 +0300

    Bump ver2

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World, V1.")
+		fmt.Fprintln(w, "Hello World, V2.")
 	})
 	log.Fatalf("error: %s", http.ListenAndServe(":8080", nil))
 }
